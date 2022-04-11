0f3ac18af42db892e3de34b831d3fbccc091876b
WebMaster <umrec_web@unimex.edu.mx>
2022-04-11 14:05:52 -0500

    return respuesta

--- src/js/service/peticiones.js
+++ src/js/service/peticiones.js
@@ -1,11 +1,16 @@
-const url = 'http://192.168.1.123/api/prospectacion/procesa-datos.php';
+//const url = 'http://192.168.1.123/api/prospectacion/procesa-datos.php';
+const url = 'https://api.unimex.edu.mx/api/prospectacion/procesa-datos.php';
+//const url = './procesa-datos.php';
 
 
 const registraProspecto = async( datosPros )=>{
 
     try {
+
+        //return datosPros;
         
         //await ({ datosPros });
+        
         const response = await fetch(url, {
             method: 'POST',
             headers: {
@@ -20,8 +25,8 @@ const registraProspecto = async( datosPros )=>{
         if( !response.ok ) throw 'Error al enviar los datos';
     
         const respuesta = await response.json();
-       console.log( respuesta );
-        
+        return respuesta;
+                
     } catch (error) {
 
         throw error;
