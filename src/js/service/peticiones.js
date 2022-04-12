f26afb8ea137159c668f7661ce10fc9021e4885e
WebMaster <umrec_web@unimex.edu.mx>
2022-04-12 16:54:11 -0500

    change fetch url

--- src/js/service/peticiones.js
+++ src/js/service/peticiones.js
@@ -1,7 +1,7 @@
 //const url = 'http://192.168.1.123/api/prospectacion/procesa-datos.php';
 //const url = 'https://api.unimex.edu.mx/api/prospectacion/procesa-datos.php';
-//const url = './php/procesa-datos.php';
-const url = 'http://192.168.1.123/dist-pros/php/procesa-datos.php';
+const url = './php/procesa-datos.php';
+//const url = 'http://192.168.1.123/dist-pros/php/procesa-datos.php';
 
 
 const registraProspecto = async( datos )=>{
