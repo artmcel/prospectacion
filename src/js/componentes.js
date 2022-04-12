b38e0201d062f0464ded44de317ea665d67b40e4
WebMaster <umrec_web@unimex.edu.mx>
2022-04-12 13:14:36 -0500

    logica para validar, y enviar datos en un objeto

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -2,75 +2,69 @@ import Swal from "sweetalert2";
 import { registraProspecto } from "./service/peticiones";
 
 
-const frm = document.getElementById('formulario'),
-    btn = document.getElementById('boton'),
-    nombre = document.getElementById('nombre'),
-    celular = document.getElementById('celular'),
-    telCasa = document.getElementById('casa'),
-    email = document.getElementById('email'),
-    nivel = document.getElementById('nivel'),
-    carrera = document.getElementById('carrera'),
-    horario = document.getElementById('horario'),
-    periodo = document.getElementById('periodo'),
-    plantel = document.getElementById('plantel'),
-    escuela = document.getElementById('escuela'),
-    nombre2 = document.getElementById('nombre2'),
-    telefono2 = document.getElementById('telefono2');
+const btn = document.getElementById('boton');
 
-let prospecto = [];
 
 btn.addEventListener('click', (e)=>{
-
-
-    e.preventDefault();
     
-    if (!nombre.value) throw alerta( nombre );
-        prospecto.unshift( nombre.value );
-    if (!celular.value) throw alerta( celular );
-        prospecto.unshift( celular.value );
-    if (!telCasa.value) throw alerta( telCasa );
-        prospecto.unshift( telCasa.value );
-    if (!email.value) throw alerta( email );
-        prospecto.unshift( email.value );
-        prospecto.unshift(nivel.value);
-    if (!carrera.value) throw alerta( carrera );
-        prospecto.unshift( carrera.value );
-    if (!horario.value) throw alerta( horario );
-        prospecto.unshift( horario.value );
-    if (!periodo.value) throw alerta( periodo );
-        prospecto.unshift( periodo.value );
-    if (!plantel.value) throw alerta( plantel );
-        prospecto.unshift( plantel.value );
-    if (!escuela.value) throw alerta( escuela );
-        prospecto.unshift( escuela.value );
-    prospecto.unshift( nombre2.value );
-    prospecto.unshift( telefono2.value );
+    const nombre = document.getElementById('nombre'),
+        celular = document.getElementById('celular'),
+        telCasa = document.getElementById('casa'),
+        email = document.getElementById('email'),
+        nivel = document.getElementById('nivel'),
+        carrera = document.getElementById('carrera'),
+        horario = document.getElementById('horario'),
+        periodo = document.getElementById('periodo'),
+        plantel = document.getElementById('plantel'),
+        escuela = document.getElementById('escuela'),
+        nombre2 = document.getElementById('nombre2'),
+        telefono2 = document.getElementById('telefono2');
         
+    if (!nombre.value) throw alerta( nombre );
+    if (!celular.value) throw alerta( celular );
+    if (!telCasa.value) throw alerta( telCasa );
+    if (!email.value) throw alerta( email );
+    if (!carrera.value) throw alerta( carrera );
+    if (!horario.value) throw alerta( horario );
+    if (!periodo.value) throw alerta( periodo );
+    if (!plantel.value) throw alerta( plantel );
+    if (!escuela.value) throw alerta( escuela );
 
+    let prospecto = {
+        
+        'nombre' : nombre.value,
+        'celular' : celular.value,
+        'telCasa' : telCasa.value,
+        'email' : email.value,
+        'nivel' : nivel.value,
+        'carrera' : carrera.value,
+        'horario' : horario.value,
+        'periodo' : periodo.value,
+        'plantel' : plantel.value,
+        'escuela' : escuela.value,
+        'nombre2' : nombre2.value,
+        'telefono2' : telefono2.value
+    }
 
     validaciones( prospecto );
 
-
-
-
 });
+    
 
 const validaciones = (datosProspecto)=>{
-    //console.log( ({datosProspecto}) );
     registraProspecto( datosProspecto ).then( resp =>{
-        console.log( resp );
+        console.log( resp.registro );
+        resp.registro === true ? window.location.href = 'registro' :  console.error('no se registro');
     });
 }
 
 const alerta = ( input )=>{
-    //alert(`el campo ${input.name}, esta vacio`);
     Swal.fire({
         icon: 'error',
         html: `El campo <strong>${input.name}</strong>, esta vacio.`
     })
     return false;
 }
-
 
 export {
     validaciones
