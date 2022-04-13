21f608ce6318f2567c00a1e0dce36a0d4d2915fd
WebMaster <umrec_web@unimex.edu.mx>
2022-04-13 10:14:12 -0500

    ad lodash

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import "regenerator-runtime/runtime";
 import "core-js/stable";
-import { validaciones } from './js/componentes';
+//import { validaciones } from './js/componentes';
 import './styles.css';
+/**
+ * lazyload
+ */
+import _ from 'lodash';
 
 
-validaciones;
+import( './js/componentes' ).then( module =>{
+    const valida = module.validaciones;
+    valida();
+})
+//validaciones;
 
 
