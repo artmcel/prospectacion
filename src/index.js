a7daea245800f5e93ecd21d7e2930338102ff237
WebMaster <umrec_web@unimex.edu.mx>
2022-04-14 11:46:49 -0500

    add comoponents login for buton

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-import _ from 'lodash';
-import print from './js/componentes';
-import './styles.css';
+/**
+ * 
+ * importamos sweet alert para prueba de oprimizacion con splitChunks...
+ * splitChunks: {all}.. separa los librerias importadas y las carga en un archivo vendors....
+ * 
+ * 
+ * 
+ */
+ import _ from 'lodash';
+ //import Swal from 'sweetalert2', add prefetch;
+ import(/*webpackPrefetch: true */ 'sweetalert2');
+ /**
+  * carga estatica de modulos...
+  * 
+  * import { saludo } from './services/peticiones';
+  */
+ 
+ 
+ //cargadinamica
+ //const mostrarMensaje = () => import( /* webpackChunkName: "peticiones" */ './services/peticiones' );
+ 
+ 
+ 
+ const boton = document.getElementById('btn');
 
-
-print;
+boton.addEventListener('click', ()=>{
+     
+     let n = 'Arturo';
+     //lazyload modules...
+     import(/*webpackChunkName: "componentes" */ './js/componentes').then( module =>{
+         const mostrarMensaje = module.saludo;
+ 
+         console.log( mostrarMensaje(n))
+     });
+ 
+})
+ 
