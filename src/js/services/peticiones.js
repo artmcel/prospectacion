abad5de10e12b74579a80b6e553cab610e632d08
WebMaster <umrec_web@unimex.edu.mx>
2022-04-14 13:40:37 -0500

    add peticiones logic

--- src/js/services/peticiones.js
+++ src/js/services/peticiones.js
@@ -1,3 +1,36 @@
 /**
  * archivo para peticiones fetch..
  */
+//const url = 'http://192.168.1.123/api/prospectacion/procesa-datos.php';
+//const url = 'https://api.unimex.edu.mx/api/prospectacion/procesa-datos.php';
+const url = './php/procesa-datos.php';
+//const url = 'http://192.168.1.123/dist-pros/php/procesa-datos.php';
+
+
+const registraProspecto = async( datos )=>{
+
+    try {
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Accept"       : "application/json",
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(datos)
+        });
+    
+        if( !response.ok ) throw 'Error al enviar los datos';
+        return await response.json();
+        
+    } catch (error) {
+
+        throw error;
+        
+    }
+
+}
+
+export {
+    registraProspecto
+}
