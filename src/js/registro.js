3f80f3b2824abf167c09fb5ae195a88b98461f10
WebMaster <umrec_web@unimex.edu.mx>
2022-04-19 13:14:50 -0500

    import logo script in registro page

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,2 +1,8 @@
 import (/*webpackPreload: true*/ '../styles.css');
 import (/*webpackPreload: true*/ 'bootstrap/dist/css/bootstrap.min.css');
+
+import( './logo' ).then( module =>{
+
+    const cambiaLogo = module.cambioLogo;
+    cambiaLogo();
+});
