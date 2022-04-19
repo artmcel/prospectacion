263ee74c20f1fefd58f788141e1a66cf106c040c
WebMaster <umrec_web@unimex.edu.mx>
2022-04-19 09:40:05 -0500

    update api url

--- src/js/services/peticiones.js
+++ src/js/services/peticiones.js
@@ -2,8 +2,8 @@
  * archivo para peticiones fetch..
  */
 //const url = 'http://192.168.1.123/api/prospectacion/procesa-datos.php';
-//const url = 'https://api.unimex.edu.mx/api/prospectacion/procesa-datos.php';
-const url = './php/procesa-datos.php';
+const url = 'https://api.unimex.edu.mx/api/prospectacion/procesa-datos.php';
+//const url = './php/procesa-datos.php';
 //const url = 'http://192.168.1.123/dist-pros/php/procesa-datos.php';
 
 
