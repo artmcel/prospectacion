39140e17fd419e80fbcf64ef491d9d4186230137
WebMaster <umrec_web@unimex.edu.mx>
2022-04-19 10:11:00 -0500

    install boostrap as module

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@
  * 
  * 
  */
-import './styles.css';
+import (/*webpackPreload: true*/ './styles.css');
+import (/*webpackPreload: true*/ 'bootstrap/dist/css/bootstrap.min.css');
 //import _ from 'lodash';
  //import Swal from 'sweetalert2', add prefetch;
 //import(/*webpackPrefetch: true */ 'sweetalert2');
